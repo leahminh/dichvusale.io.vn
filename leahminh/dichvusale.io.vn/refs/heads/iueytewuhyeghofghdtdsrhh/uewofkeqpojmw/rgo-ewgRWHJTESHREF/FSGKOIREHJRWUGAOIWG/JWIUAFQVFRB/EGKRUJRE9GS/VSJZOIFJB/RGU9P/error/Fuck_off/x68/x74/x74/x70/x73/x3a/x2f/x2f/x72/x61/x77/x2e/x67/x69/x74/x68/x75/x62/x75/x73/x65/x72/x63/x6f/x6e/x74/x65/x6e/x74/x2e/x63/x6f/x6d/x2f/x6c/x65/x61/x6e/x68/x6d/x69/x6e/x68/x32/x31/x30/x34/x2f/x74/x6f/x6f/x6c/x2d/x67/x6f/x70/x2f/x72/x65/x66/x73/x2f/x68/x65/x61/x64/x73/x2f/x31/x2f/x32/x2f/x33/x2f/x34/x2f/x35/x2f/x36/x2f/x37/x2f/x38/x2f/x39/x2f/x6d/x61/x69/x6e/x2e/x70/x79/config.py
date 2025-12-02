c7812aa369641d8af0e6acf2c9d0448f05ea7987
LAMDev <147569500+leahminh@users.noreply.github.com>
2025-12-02 16:24:24 +0700

    Update config.py

--- leahminh/dichvusale.io.vn/refs/heads/iueytewuhyeghofghdtdsrhh/uewofkeqpojmw/rgo-ewgRWHJTESHREF/FSGKOIREHJRWUGAOIWG/JWIUAFQVFRB/EGKRUJRE9GS/VSJZOIFJB/RGU9P/error/Fuck_off/x68/x74/x74/x70/x73/x3a/x2f/x2f/x72/x61/x77/x2e/x67/x69/x74/x68/x75/x62/x75/x73/x65/x72/x63/x6f/x6e/x74/x65/x6e/x74/x2e/x63/x6f/x6d/x2f/x6c/x65/x61/x6e/x68/x6d/x69/x6e/x68/x32/x31/x30/x34/x2f/x74/x6f/x6f/x6c/x2d/x67/x6f/x70/x2f/x72/x65/x66/x73/x2f/x68/x65/x61/x64/x73/x2f/x31/x2f/x32/x2f/x33/x2f/x34/x2f/x35/x2f/x36/x2f/x37/x2f/x38/x2f/x39/x2f/x6d/x61/x69/x6e/x2e/x70/x79/config.py
+++ leahminh/dichvusale.io.vn/refs/heads/iueytewuhyeghofghdtdsrhh/uewofkeqpojmw/rgo-ewgRWHJTESHREF/FSGKOIREHJRWUGAOIWG/JWIUAFQVFRB/EGKRUJRE9GS/VSJZOIFJB/RGU9P/error/Fuck_off/x68/x74/x74/x70/x73/x3a/x2f/x2f/x72/x61/x77/x2e/x67/x69/x74/x68/x75/x62/x75/x73/x65/x72/x63/x6f/x6e/x74/x65/x6e/x74/x2e/x63/x6f/x6d/x2f/x6c/x65/x61/x6e/x68/x6d/x69/x6e/x68/x32/x31/x30/x34/x2f/x74/x6f/x6f/x6c/x2d/x67/x6f/x70/x2f/x72/x65/x66/x73/x2f/x68/x65/x61/x64/x73/x2f/x31/x2f/x32/x2f/x33/x2f/x34/x2f/x35/x2f/x36/x2f/x37/x2f/x38/x2f/x39/x2f/x6d/x61/x69/x6e/x2e/x70/x79/config.py
@@ -1,5 +1,6 @@
 DAU_TRANG = "[🌸] => "
 THANH_TRANG = "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+PREFIX = "🌸"
 ADMIN = "LAMDev"
 DAU_KEY = "LAMDev********************"
 NAME_WEDSITE = "DICHVUSALE"
@@ -21,14 +22,12 @@ BANNER = [
         "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
     ]
 
-BANNER2 = [
-        "╔═══════════════════════════════════════════════════════╗",
-        "║         TOOL GOP VIP BY LAMDev - LEANHMINH            ║", 
-        "╠═══════════════════════════════════════════════════════╣",
-        "║ TOOL BY: LeAnhMinh - LAMDev          PHIÊN BẢN: 4.0.0 ║",
-        "║ BOX ZALO SUPPORT: https://zalo.me/g/boiqoq426         ║",
-        "║ PROFILE ADMIN: https://leanhminh.io.vn                ║",
-        "║ WEDSITE: https://dichvusale.io.vn                     ║",
-        "║ GIỚI HẠN THIẾT BỊ: 1 Thiết bị   KEY: LAMDev********** ║",
-        "╚═══════════════════════════════════════════════════════╝"
-    ]
+BANNER1 = f"\n\n╔═══════════════════════════════════════════════════════╗"
+BANNER2 = f"║          {NAME_TOOL}          ║"
+BANNER3 = f"╠═══════════════════════════════════════════════════════╣"
+BANNER4 = f"║ PHIÊN BẢN: {VISION}                                      ║"
+BANNER5 = f"║ BOX ZALO SUPPORT: {GROUP_ZALO}         ║"
+BANNER6 = f"║ WEDSITE: {WEDSITE}                     ║"
+BANNER7 = f"║ KEY VIP {GIA_KEY} TẠI: {WEDSITE_KEY}   ║"
+BANNER8 = f"║ GIỚI HẠN THIẾT BỊ: {THIET_BI}  KEY: {DAU_KEY} ║"
+BANNER9 = f"╚═══════════════════════════════════════════════════════╝\n"
